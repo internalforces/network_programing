f75e9f1198b2165b60c7cfa88d8284217996427e
internalforces <81242244+internalforces@users.noreply.github.com>
2025-03-12 13:01:33 +0900

    네트워크 프로그래밍 과제

--- src/ReadCharactersIncr.java
+++ src/ReadCharactersIncr.java
@@ -1,26 +1,32 @@
 import java.io.*;
 
 class ReadCharactersIncr {
-    static int size =0;
-    static int bufferSize=80;
+    static int size = 0;
+    static int bufferSize = 80;
     static byte buffer[] = new byte[bufferSize];
-    public static void main(String[] args) throws java.io.IOException {
+
+    public static void main(String[] args) throws IOException {
         try {
             int dataRead;
-            while((dataRead=System.in.read(buffer,size,bufferSize-size))>=0) {
-                size+=dataRead;
-                if(size==bufferSize)
+            while ((dataRead = System.in.read(buffer, size, bufferSize - size)) >= 0) {
+                size += dataRead;
+                if (size == bufferSize)
                     increaseBufferSize();
             }
-            System.out.write(buffer,0,size);
-        }catch(IOException e) {
+            System.out.write(buffer, 0, size);
+        } catch (IOException e) {
             System.err.println("스트림으로부터 데이터를 읽을 수 없습니다.");
         }
     }
+
     static void increaseBufferSize() {
-        bufferSize+=80;
-        byte[] newBuffer=new byte[bufferSize];
-        System.arraycopy(buffer, 0, newBuffer, 0, size);
-        buffer = newBuffer; // buffer 배열이 크기가 80개 늘어난다.
+        bufferSize += 80;
+        byte[] newBuffer = new byte[bufferSize];
+        copyArray(buffer, 0, newBuffer, 0, size);
+        buffer = newBuffer;
+    }
+
+    static void copyArray(byte[] src, int srcPos, byte[] dest, int destPos, int length) {
+        System.arraycopy(src, srcPos, dest, destPos, length);
     }
 }
